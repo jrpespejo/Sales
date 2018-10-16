f31d7ec123d14da2499491776d525e4fd5164ce5
jrpespejo <jricardoespejo@gmail.com>
2018-10-15 22:36:58 -0500

    Proyecto Conectado Base Datos y Producto
    Catalogo de productos creado, referencia de projectos, Backeend

---
+++ Sales.Common/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime PublisOn { get; set; }
+
+    }
+}
