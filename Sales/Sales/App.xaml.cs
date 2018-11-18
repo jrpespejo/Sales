3e030dbf634124441fbeea246ff86c943b2603dd
jrpespejo <jricardoespejo@gmail.com>
2018-11-17 19:28:36 -0500

    Actualization

--- Sales/Sales/App.xaml.cs
+++ Sales/Sales/App.xaml.cs
@@ -10,6 +10,8 @@ namespace Sales
     using Sales.Helpers;
     using Newtonsoft.Json;
     using Sales.Common.Models;
+    using System.Threading.Tasks;
+    using Sales.Services;
 
     public partial class App : Application
     {
@@ -39,6 +41,42 @@ namespace Sales
 
 
         }
+        public static Action HideLoginView
+        {
+            get
+            {
+                return new Action(() => Current.MainPage = new NavigationPage(new LoginPage()));
+            }
+        }
+
+        public static async Task NavigateToProfile(TokenResponse token)
+        {
+            if (token == null)
+            {
+                Application.Current.MainPage = new NavigationPage(new LoginPage());
+                return;
+            }
+
+            Settings.IsRemembered = true;
+            Settings.AccesToken = token.AccessToken;
+            Settings.TokenType = token.TokenType;
+
+            var apiService = new ApiService();
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlUsersController"].ToString();
+            var response = await apiService.GetUser(url, prefix, $"{controller}/GetUser", token.UserName, token.TokenType, token.AccessToken);
+            if (response.IsSuccess)
+            {
+                var userASP = (MyUserASP)response.Result;
+                MainViewModel.Getinstance().UserASP = userASP;
+                Settings.UserASP = JsonConvert.SerializeObject(userASP);
+            }
+
+            MainViewModel.Getinstance().Products = new ProductsViewModel();
+            Application.Current.MainPage = new MasterPage();
+        }
+
 
         protected override void OnStart()
         {
--- Sales/Sales/ViewModels/MainViewModel.cs
+++ Sales/Sales/ViewModels/MainViewModel.cs
@@ -38,13 +38,24 @@ namespace Sales.ViewModels
         {
             get
             {
-                if (this.UserASP != null && this.UserASP.Claims != null && this.UserASP.Claims.Count >3)
+                foreach (var claim in this.UserASP.Claims)
                 {
-                    return $"https://salesapi1.azurewebsites.net{this.UserASP.Claims[3].ClaimValue.Substring(1)}";
+                    if (claim.ClaimType == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/uri")
+                    {
+                        if (claim.ClaimValue.StartsWith("~"))
+                        {
+                            //return $"https://salesapiservices.azurewebsites.net{claim.ClaimValue.Substring(1)}";
+                            return $"https://salesapi1.azurewebsites.net{claim.ClaimValue.Substring(1)}";
+                        }
+
+                        return claim.ClaimValue;
+                    }
                 }
 
                 return null;
             }
+
+           
         }
         #endregion
         #region Constructors
